299195e0b2f7b3c3044c8adeddc2cddb6b7edc51
HMIMID <ahmedhmimid177@gmail.com>
2024-11-04 16:27:50 +0100

    feat: updating index.js file

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+let bodyParser = require("body-parser");
+const URL = require('url').URL;
+let dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -17,6 +20,36 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.post("/api/shorturl", (req,res) => {
+  const originalURL = req.body.url;
+  try {
+  const urlObject = new URL(originalURL);
+  dns.lookup(urlObject.hostname, (err) => {
+    if (err) {
+      res.json({ error: 'invalid url' });
+    } else {
+      res.json({ original_url: req.body.url, short_url: Math.floor(Math.random() * 100000) });
+    }
+  });
+  } catch (er) {
+    res.json({ error: 'invalid url' });
+  }
+});
+
+app.get("/api/shorturl/:number?", (req,res) => {
+    if( !Number.isNaN(Number(req.params.number))) {
+      if(Number(req.params.number) !== 0) {
+        res.redirect('back');
+      } else {
+        res.json({ error: 'Wrong format' });
+      } 
+    } else {
+      res.json({ error: 'Wrong format' });
+    }
 });
 
 app.listen(port, function() {
